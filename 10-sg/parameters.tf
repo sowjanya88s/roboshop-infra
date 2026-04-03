a72eb97aa210ed4b522366f44e74ff272a2621df
sowjanya <devopssowjanya88s@gmail.com>
2026-04-03 18:11:00 +0530

    new

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "sg_ids" {
-  count = length[var.sg_names]
+  count = length(var.sg_names)
   name  = "/${var.project}/${var.environment}/var.sg_names[count.index]"
   type  = "String"
-  value = module.sg.mongodb_sg_id
-}
+  value = module.sg.sg_ids
+  }
