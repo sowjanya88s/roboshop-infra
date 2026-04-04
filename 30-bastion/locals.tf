e1f7276ec30c80aa15dd4abf616ed7a945f38dab
sowjanya <devopssowjanya88s@gmail.com>
2026-04-04 23:10:35 +0530

    new

--- 30-bastion/locals.tf
+++ 30-bastion/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
     public_subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
     common_tags = {
         project = "roboshop"
