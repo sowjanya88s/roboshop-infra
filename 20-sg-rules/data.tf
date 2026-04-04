b7f04a8eafc1579c43205b6c6ef3f937a8b9de96
sowjanya <devopssowjanya88s@gmail.com>
2026-04-04 22:17:28 +0530

    new

---
+++ 20-sg-rules/data.tf
@@ -0,0 +1,18 @@
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "/${var.project}/${var.environment}/bastion_sg_id"
+}
+data "http" "my_public_ip" {
+  url = "https://icanhazip.com"
+}
+output "my_ip" {
+  value = local.my_public_ip
+}
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project}/${var.environment}/mongodb_sg_id"
+}
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
+data "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project}/${var.environment}/user_sg_id"
+}
---
+++ 20-sg-rules/locals.tf
@@ -0,0 +1,7 @@
+locals {
+    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    my_public_ip = "${chomp(data.http.my_public_ip.response_body)}/32"
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    user_sg_id = data.aws_ssm_parameter.user_sg_id.value
+}
