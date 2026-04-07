28e6c7995741de3dd1c70da3ce44f7ffe43ca188
sowjanya <devopssowjanya88s@gmail.com>
2026-04-07 22:17:51 +0530

    new

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,6 +2,7 @@
 component = $1
 
 dnf install ansible -y
+cd /home/ec2-user
 git clone https://github.com/sowjanya88s/ansible-roboshop-roles-tf.git
 cd ansible-roboshop-roles-tf
 ansible-playbook -e component=$component roboshop.yaml
