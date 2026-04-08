ebddd15a9b2114af2360ea46fff5fe2a967a7aa9
sowjanya <devopssowjanya88s@gmail.com>
2026-04-08 21:09:40 +0530

    new

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -3,7 +3,7 @@
 # We are creating 50GB root disk, but only 20GB is partitioned
 # Remaining 30GB we need to extend using below commands
 growpart /dev/nvme0n1 4
-lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+lvextend -r -L +20G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
 
 yum install -y yum-utils
