38dc675a3d57edeb5f6649629edc7d582999070f
sowjanya <devopssowjanya88s@gmail.com>
2026-04-09 20:21:37 +0530

    new

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -61,3 +61,21 @@ resource "aws_security_group_rule" "mysql_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.mysql_sg_id
 }
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  # cidr_blocks       = ["0.0.0.0/0"]
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # cidr_blocks       = ["0.0.0.0/0"]
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.catalogue_sg_id
+}
