f389b9be40d61aca41630843619bf0a314e3801a
sowjanya <devopssowjanya88s@gmail.com>
2026-04-10 20:23:54 +0530

    new

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -40,3 +40,13 @@ resource "terraform_data" "catalogue" {
     
   
 }
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+}
