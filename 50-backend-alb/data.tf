077602a7ac0844b0ff0dd8079fec8fc004991e95
sowjanya <devopssowjanya88s@gmail.com>
2026-04-12 15:08:47 +0530

    new

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -5,8 +5,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 resource "aws_ssm_parameter" "backend_alb_listener_arn" {
-  count = length(var.sg_names)
-  name  = "/${var.project}/${var.environment}/${var.backend_alb_listener_arn}"
+  name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"
   type  = "String"
   value = aws_lb_listener.http.arn
   }
