05a2fa18f61701cf07f36cc3cc80a77aa8eace2a
sowjanya <devopssowjanya88s@gmail.com>
2026-04-15 08:08:36 +0530

    new

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -29,5 +29,5 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/backend_alb_sg_id"
 }
 data "aws_ssm_parameter" "Frontend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/Frontend_alb_sg_id"
+  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
