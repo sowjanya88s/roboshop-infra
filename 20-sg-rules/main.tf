81dee9f6a1ca351cfb331ed617d7f766c3ffad8b
sowjanya <devopssowjanya88s@gmail.com>
2026-04-15 21:05:19 +0530

    new

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -206,7 +206,7 @@ resource "aws_security_group_rule" "backend_alb_frontend" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  source_security_group_id = local.fronend_sg_id
+  source_security_group_id = local.frontend_sg_id
   security_group_id = local.backend_alb_sg_id
 }
 
