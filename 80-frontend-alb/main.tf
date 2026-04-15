63385ea9a76fd8d3f017e019f18072f9af8ae40a
sowjanya <devopssowjanya88s@gmail.com>
2026-04-15 12:22:55 +0530

    new

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "fronend_alb" {
   name               = "${var.project}-${var.environment}-fronend-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [local.fronend_alb_sg_id]
+  security_groups    = [local.frontend_alb_sg_id]
   subnets            = local.public_subnet_ids
 
   enable_deletion_protection = false
