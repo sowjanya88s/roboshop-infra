8d26bbc5ccd1f73f1c0c8a878d803b8bbd540670
sowjanya <devopssowjanya88s@gmail.com>
2026-04-19 12:44:17 +0530

    new

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -302,8 +302,8 @@ resource "aws_security_group_rule" "backend_alb_openvpn" {
   to_port           = 80
   protocol          = "tcp"
   # Where traffic is coming from
- source_security_group_id = local.backend_alb_sg_id
-  security_group_id = local.openvpn_sg_id
+ source_security_group_id = local.openvpn_sg_id
+  security_group_id = local.backend_alb_sg_id
 }
 resource "aws_security_group_rule" "openvpn" {
   type              = "ingress"
