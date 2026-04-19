7865b0aaeb1143628c04d1826cfeb62944619ce2
sowjanya <devopssowjanya88s@gmail.com>
2026-04-19 12:11:42 +0530

    new

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -47,5 +47,5 @@ data "aws_ssm_parameter" "frontend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
 data "aws_ssm_parameter" "openvpn_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/openvpn_alb_sg_id"
+  name = "/${var.project}/${var.environment}/openvpn_sg_id"
 }
